39ac25a3779fc7c0099077d6ff08e0de06893d76
Hmvp <github@hmvp.nl>
2013-05-09 17:24:05 +0200

    simple ev meuk

--- simpleEV/models.py
+++ simpleEV/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 
 class DocumentLabels(models.Model):
-    document = models.OneToOneField('core.Document')
+    document = models.OneToOneField('core.Document', related_name='label')
     label1 = models.CharField(max_length=100)
     label2 = models.CharField(max_length=100)
     label3 = models.CharField(max_length=100)
     label4 = models.CharField(max_length=100)
     label5 = models.CharField(max_length=100)
+
+
+class DocumentLabels2(models.Model):
+    document = models.ForeignKey('core.Document', related_name='labels')
+    label = models.CharField(max_length=100)
+    volgorde = models.PositiveSmallIntegerField()
